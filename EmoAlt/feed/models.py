a936fb6bb7c084b1e0a581be87b54b4022cfb664
= <lkrausha@ualberta.ca>
2023-11-17 17:57:25 -0700

    models

--- EmoAlt/feed/models.py
+++ EmoAlt/feed/models.py
@@ -2,3 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class BrainWave(models.Model):
+    name = models.CharField(max_length=400)
+
+class WavePoint(models.Model):
+    wave = models.ForeignKey(BrainWave, on_delete=models.CASCADE)
+
+class WaveTime(models.Model):
+    point = models.ForeignKey(WavePoint, on_delete=models.CASCADE)
+    time = models.FloatField()
+
+class WaveValue(models.Model):
+    point = models.ForeignKey(WavePoint, on_delete=models.CASCADE)
+    value = models.FloatField()
